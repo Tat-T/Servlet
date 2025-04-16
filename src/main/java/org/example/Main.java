4b4d2c9a81b63ec05765b4b82b7e2ebc62458562
Tat-T <ring_87_87@mail.ru>
2025-04-16 19:02:54 +0300

    работает

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+// строка с декоратором
 @WebServlet("/hello")
 public class Main extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-      // response.setContentType("text/html;charset=UTF-8");
+        // если не использовать строку декоратора
+       // response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter();) {
             out.println("<h1>Hello world</h1>");
         }
